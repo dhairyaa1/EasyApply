dc76a1d45d6c01c1d0fefb48ee85703a1c5739af
Dhairyaa <dhairyaaparikh18@gmail.com>
2024-07-04 08:21:23 +0530

    Updated credentials in loginservice

--- LoginService/src/main/java/com/easyapply/loginservice/configuration/AppConfiguration.java
+++ LoginService/src/main/java/com/easyapply/loginservice/configuration/AppConfiguration.java
@@ -1,5 +1,6 @@
 package com.easyapply.loginservice.configuration;
 
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,11 +9,8 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class AppConfiguration {
-
-
-
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity security) throws Exception {
-        return security.csrf(AbstractHttpConfigurer::disable).authorizeRequests(expressionInterceptUrlRegistry -> expressionInterceptUrlRegistry.anyRequest().hasIpAddress("0:0:0:0:0:0:0:1")).csrf(AbstractHttpConfigurer::disable).build();
+        return security.csrf(AbstractHttpConfigurer::disable).authorizeRequests(expressionInterceptUrlRegistry -> expressionInterceptUrlRegistry.anyRequest().access("hasIpAddress(\"127.0.0.1\") or hasIpAddress(\"::1\")")).build();
     }
 }
